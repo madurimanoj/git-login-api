07b66d76248cbb09edee5873ae7ca1342b408090
mbr84 <matthew.bramfeld@gmail.com>
2017-06-20 03:06:35 -0700

    pg pool

--- routes/index.js
+++ routes/index.js
@@ -22,20 +22,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'GitHub User Search' });
 });
 
+pg.defaults.ssl = true;
 router.get('/api/users/:partialLogin', function(req, res, next) {
-  pool.connect(process.env.DATABASE_URL, function(err, client) {
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
 
   client
-    .query("SELECT login, similarity(login, $1) AS sml FROM users WHERE login % $1 ORDER BY sml DESC, login LIMIT 12;", [req.params.partialLogin])
-      .then(data => {
-        res.status(200)
-          .json({
-            status: 'success',
-            data,
-          })
-      })
-    });
+  .query("SELECT login, similarity(login, $1) AS sml FROM users WHERE login % $1 ORDER BY sml DESC, login LIMIT 12;", [req.params.partialLogin])
+    .then(data => {
+      res.status(200)
+        .json({
+          status: 'success',
+          data,
+        })
+    })
+  });
 });
+
 
 module.exports = router;
