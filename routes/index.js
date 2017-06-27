3d0b2efc41143940b4aec882e5eaef9899f1c28d
mbr84 <matthew.bramfeld@gmail.com>
2017-06-26 18:46:38 -0700

    end pg connection

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,21 @@ router.get('/', function(req, res, next) {
 
 pg.defaults.ssl = true;
 router.get('/api/users/:partialLogin', function(req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) throw err;
 
-  client
-  .query("SELECT login, similarity(login, $1) AS sml FROM users WHERE login % $1 ORDER BY sml DESC, login LIMIT 12;", [req.params.partialLogin])
-    .then(data => {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data.rows
-        })
-    })
+    client
+    .query("SELECT login, similarity(login, $1) AS sml FROM users WHERE login % $1 ORDER BY sml DESC, login LIMIT 12;", [req.params.partialLogin])
+      .then(data => {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data.rows
+          })
+      })
+    done()
   });
+  pg.end()
 });
 
 
